1960002bf478688474fed4e2906319411af194fb
Sakika0704 <sakikahamada74u@gmail.com>
2022-07-08 20:01:01 +0900

    wip-20220708

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,62 +10,86 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main>
+        <div className='font-body tracking-widest mx-40'>
+          <div className='flex flex-col-reverse md:flex-row justify-center px-4 md:px-0'>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+            <div>
+              <p className="text-lg mb-4 font-bold ">
+                大学や街を遊び歩く
+              </p>
+              <h1 className='text-4xl mb-4 font-bold'>
+                地域に根差す
+              </h1>
+              <h1 className='text-4xl mb-4 font-bold'>
+                テクノロジー集団
+              </h1>
+              <p className='tracking-wide leading-loose w-88'>
+                学生団体U-labは、宇都宮大学の学生が中心となって、デザインやテクノロジーを用いて街や大学に対して面白いことを企て、実行する学生団体です。 圧倒的なインプット環境、普通では参加できないインターン、大規模なチームプロジェクトなどを行い、宇都宮大学で最も勢いのある学生団体を目指します。 また、定期的なゲーム会やご飯会などを通し、日々仲を深めています。
+              </p>
+            </div>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className='w-80 h-80'>
+              <img alt="学生団体U-lab" src="ulab-touka.webp" />
+            </div>
+          </div>
         </div>
+
+
+        <div>
+          <div>
+            <p>U-labの</p>
+            <h2>活動内容</h2>
+          </div>
+
+          <div>
+            <h1>01</h1>
+            <p>不定期で誰でも開催・参加することができる勉強会</p>
+            <p>U-labでは誰でも自由に、勉強したことや興味のあることを教え合う勉強会を開催したり、他の人の開いた勉強会に参加したりすることができます。</p>
+          </div>
+
+          <div>
+            <h1>02</h1>
+            <p>異なる専門の学生同士で企むチームプロジェクト</p>
+            <p>U-labでは、チームで協力して様々な面白いことを企てるチームプロジェクトというものがあります。大会やイベントに参加することもあります。</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <p>U-labの</p>
+            <h2>代表的なプロジェクト</h2>
+          </div>
+
+          <div>
+            <p>バーチャル宇都宮大学</p>
+            <p>新入生や高校生がコロナ禍で大学に来れない問題を解決するため、仮想空間にバーチャルな宇都宮大学を作成し、高校生に向けたオープンキャンパスを行いました。</p>
+          </div>
+
+          <div>
+            <p>UU-Circles</p>
+            <p>新入生がオンラインでも宇大のサークルを見つけることができるように、最新のweb技術を用いて、企画から構築・運用までを行いました。</p>
+          </div>
+
+          <div>
+            <p>uu-yell</p>
+            <p>新入生が宇都宮大学の情報を得ることが難しいという問題を解決するため、宇大生が中心となって、宇都宮大学生向けのメディアサイトを運営しています。</p>
+          </div>
+        </div>
+
+
+        <div>
+          <p>入部希望はこちらから</p>
+        </div>
+
+
+
+
+
+
+
+
+
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
